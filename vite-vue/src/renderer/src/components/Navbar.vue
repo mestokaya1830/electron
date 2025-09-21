5917f5b5d952b9b5c968b5cc22b3d3cc5cf198ec
mestokaya1830 <mesto1830@outllok.com>
2025-09-21 18:06:46 +0200

    mkm

--- vite-vue/src/renderer/src/components/Navbar.vue
+++ vite-vue/src/renderer/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template lang="">
   <div>
-    <router-link to="/" exact-active-class="active">Home</router-link>
-    <router-link to="/login" exact-active-class="active">Login</router-link>
-    <router-link to="/register" exact-active-class="active">Register</router-link>
+    <router-link to="/" class="links" exact-active-class="active">Home</router-link>
+    <router-link to="/login" class="links" exact-active-class="active">Login</router-link>
+    <router-link to="/register" class="links" exact-active-class="active">Register</router-link>
   </div> 
 </template>
