4b9b0ec1b7046854635da224a60b4c0f4dce3401
mestokaya1830 <mesto1830@outllok.com>
2025-09-21 02:55:34 +0200

    mkmm

--- vite-react/src/renderer/src/pages/Home.jsx
+++ vite-react/src/renderer/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 
 const getUsers = async () => {
-  const res = window.api.getUsers()
+  const res = await window.api.getUsers()
   console.log(res)
 }
 export default function Home() {
