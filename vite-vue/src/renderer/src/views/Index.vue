6dee7ae82aae31c7cc3467c0de92b220ad0410a7
mestokaya1830 <mesto1830@outllok.com>
2025-09-21 18:00:38 +0200

    mkmk

--- vite-vue/src/renderer/src/views/Index.vue
+++ vite-vue/src/renderer/src/views/Index.vue
@@ -1,6 +1,13 @@
 <template lang="">
   <div>
     <h1>{{ title }}</h1>
+    <div v-if="$store.state.auth">
+      <h3>Logged in as : {{ $store.state.auth.email }}</h3>
+      <button @click="logout">Logout</button>
+      <ul v-for="user in users" :key="user.id">
+        <li>{{ user.email }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -8,7 +15,8 @@
 export default {
   data() {
     return {
-      title: 'Home Page'
+      title: 'Home Page',
+      users: []
     }
   },
   mounted() {
@@ -19,9 +27,14 @@ export default {
       try {
         const res = await window.api.getUsers()
         console.log(res)
+        this.users = res.users
       } catch (err) {
         console.log(err)
       }
+    },
+    logout() {
+      this.$store.commit('setAuth', null)
+      this.$router.push('/login')
     }
   }
 }
