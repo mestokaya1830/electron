0b27a56d55d262ec65a7895c0921d18676d74b41
mestokaya1830 <mesto1830@outllok.com>
2025-11-30 03:36:05 +0100

    vfvf

--- vite-vue-sqlite3/src/renderer/src/components/Navbar.vue
+++
@@ -1,7 +0,0 @@
-<template lang="">
-  <div>
-    <router-link to="/" class="links" exact-active-class="active">Home</router-link>
-    <router-link to="/login" class="links" exact-active-class="active">Login</router-link>
-    <router-link to="/register" class="links" exact-active-class="active">Register</router-link>
-  </div> 
-</template>
